f4c8d9004f63e2b3f50a23bdec46996b99822a75
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-02-28 15:45:38 +0100

    Deploy ECS

--- scripts/travis/deploy_to_amazon_ecs.sh
+++ scripts/travis/deploy_to_amazon_ecs.sh
@@ -1,6 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-#ecs-cli configure --region eu-west-3 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster aws-services-dev
 ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
 ecs-cli configure --region eu-west-3 --cluster aws-services-dev
-ecs-cli compose --file scripts/docker/docker-compose.yml up
+ecs-cli compose --file scripts/docker/docker-compose.yml --project-name eurekaserver up
 rm -rf ~/.ecs
