3fe8cf4a1b608aca45175fe587d58787b848306f
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-02-28 14:08:58 +0100

    Deploy ECS

--- scripts/travis/deploy_to_amazon_ecs.sh
+++ scripts/travis/deploy_to_amazon_ecs.sh
@@ -1,5 +1,6 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 #ecs-cli configure --region eu-west-3 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster aws-services-dev
 ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure --region eu-west-3 --cluster aws-services-dev
 ecs-cli compose --file ../docker/docker-compose.yml up
 rm -rf ~/.ecs
