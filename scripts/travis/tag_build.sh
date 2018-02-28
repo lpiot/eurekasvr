d65880c7b5e9b930f63f4ac8fb92923a113f7eb6
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-02-28 12:03:50 +0100

    Deploy ECS

--- scripts/travis/tag_build.sh
+++ scripts/travis/tag_build.sh
@@ -1,6 +1,5 @@
 echo "Tagging build with $BUILD_NAME"
-#export GITHUB_TOKEN="a2dbd554f0da4d4ff815195bc345711eeb430ee4"
-export TARGET_URL="https://github.com/anthonydenecheau/eurekasvr/releases?access_token=$GITHUB_TOKEN"
+export TARGET_URL="https://api.github.com/repos/anthonydenecheau/eurekasvr/releases?access_token=$GITHUB_TOKEN"
 
 body="{
   \"tag_name\": \"$BUILD_NAME\",
