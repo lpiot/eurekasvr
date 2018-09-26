20a59eff4bb5a72c94200c8ca64359a3684ad3f5
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-09-26 14:36:49 +0200

    last update

--- scripts/travis/deploy_to_amazon_ecs.sh
+++
@@ -1,5 +0,0 @@
-echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure profile --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
-ecs-cli configure --region eu-west-3 --cluster aws-services-dev
-ecs-cli compose --file scripts/docker/docker-compose.yml --project-name eurekaserver up
-rm -rf ~/.ecs
