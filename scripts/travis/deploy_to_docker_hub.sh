6b2ed71eb296c1aadb245bf83932a9e927a2dcd8
Ludovic Piot <ludovic.piot@thegaragebandofit.com>
2019-05-28 11:11:54 +0200

    :green_heart: :whale: :lock: changed docker hub repository

--- scripts/travis/deploy_to_docker_hub.sh
+++ scripts/travis/deploy_to_docker_hub.sh
@@ -1,3 +1,3 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push anthonydenecheau/scc-eurekasvr:$BUILD_NAME
+docker push $DOCKER_USERNAME/scc-eurekasvr:$BUILD_NAME
